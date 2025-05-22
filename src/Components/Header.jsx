f02ce08189994edc12f485c181bed8e436b1e195
nileshmantati <nileshmantati@gmail.com>
2025-05-22 19:58:10 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -24,7 +24,7 @@ function Header() {
                         </ul>
                         <ul className="language mb-0 ps-0">
                             <li className='px-2 position-relative'><a href='#' className='d-flex column-gap-1 align-items-center text-decoration-none'>USD<i className="fa-solid fa-chevron-down fa-xs ms-1"></i></a>
-                                <ul className='down mb-0 p-3 position-absolute rounded-1'>
+                                <ul className='down mb-0 p-3 position-absolute rounded-1 w-100'>
                                     <li className='mb-2'><a href="#" className='d-flex column-gap-2 text-decoration-none'>EUR</a></li>
                                     <li className='mb-2'><a href="#" className='d-flex column-gap-2 text-decoration-none'>BRL</a></li>
                                 </ul>
@@ -51,27 +51,25 @@ function Header() {
                 </div>
             </div>
             <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link><Link to="/" className='home'>Home</Link></Nav.Link>
-                            <Nav.Link><Link to="">Link</Link></Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+                <Navbar.Brand href="#home"><img src={require('../img/logo-furniture.png')} alt="Not Found Logo" width={100} height={40} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link><Link to="/" className='home'>Home</Link></Nav.Link>
+                        <Nav.Link><Link to="">Link</Link></Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     )
