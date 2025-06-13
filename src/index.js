67c8cacc223c2dbfadba32f23fe6734589c47c1a
Sanjay Mantati <sanjayms9510@gmail.com>
2025-06-13 15:37:25 +0530

    Link Fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/merto_website'>
     <App />
   </BrowserRouter>
 );
