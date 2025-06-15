680c339cd7e8a1fd5e6b848f899d18db9fc5ee44
Sanjay Mantati <sanjayms9510@gmail.com>
2025-06-15 15:45:56 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './Components/index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
