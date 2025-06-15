9829ee2845853c52df56278db50595921137ed26
Sanjay Mantati <sanjayms9510@gmail.com>
2025-06-15 16:12:52 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './Components/index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/merto_website'>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
