5962750672ebe6e5fee1761ab8acfdaeefb71853
Sanjay Mantati <sanjayms9510@gmail.com>
2025-06-15 16:07:48 +0530

    Fix

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@
 // https://demo.theme-sky.com/merto/home-furniture/?color=brown&vertical_menu_style=inside&store_notice=1
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from './Homepage';
 import Shoppage from './Shoppage';
 import Productpage from './Productpage';
@@ -12,7 +12,7 @@ import Aboutuspage from './Aboutuspage';
 function App() {
   return (
     <div className="App">
-      <Router basename="/merto_website">
+      <Router>
         <Routes >
           <Route path="/" element={<Homepage />} />
           <Route path="/shoppage" element={<Shoppage />} />
